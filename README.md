aa57caec90d51c0dacadc2779b7bd928e9586a98
Juanjo M <juanjo@jmontiel.es>
2016-01-21 17:31:27 +0100

    Fixed an error because of controls do not appear when jQuery is loaded after executing oncanplay in audio element. Created  the English translation.  Renamed variables for readability.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ http://www.programad.me
 
 This little script finds all the audio elements of a page. For each  element, adds accessible controls above it, in order to   manipulate the player.
 The script requires Jquery 1.7 or above. Jquery 2.1.4 is included.
-The script is currently in Spanish, so translations are apreciated ;)
+The script is currently in Spanish and in an bad english , so translations and corrections are greatly apreciated ;)
+
+For translations, I'm using the tools from: http://www.localeplanet.com
+If you want to collaborate in translation or improving the player, feel free to do it by using the github repository: https://github.com/kastwey/html5-accessible-audio-player
 
 Enjoy it!
--- html5-accessible-audio-player.js
+++ html5-accessible-audio-player.js
@@ -1,184 +1,210 @@
 //  html5-accessible-audio-player
-// Juan JosÈ Montiel PÈrez: @kastwey.
+// Juan Jos√© Montiel P√©rez: @kastwey.
 // http://www.programad.me
 // This little script finds all the audio elements of a page.
-// For each  element, adds accessible controls above it, in order to   manipulate the player.
-// In order to add the title in links, add an attribute called "data-title" with the audio title.
+// For each  element, it adds accessible controls above it, in order to   manipulate the player.
+// to add the title in links, add an attribute called "data-title" with the audio title.
 // Additionally, you can add the data-showDownloadLink attribute to the element. If the attribute is true,
 //  a download link will be rendered above accessible controls.
 
+// Audio ReadyState constants
+const AUDIO_HAVE_NOTHING = 0, AUDIO_HAVE_METADATA = 1, AUDIO_HAVE_CURRENT_DATA = 2,
+	AUDIO_HAVE_FUTURE_DATA = 3, AUDIO_HAVE_ENOUGH_DATA = 4;
+
+
+
+// Says a text with active screen reader using aria live capabilities.
 function accessibleAlert(text, wrapper) {
 	if (wrapper.length == 0 || !wrapper.is(":visible")) return;
 	setTimeout(function () {
 		var divAlert;
-		if (wrapper.find(".parrafoEstadoAudio").length == 0) {
-			var parrafoMensaje = $('<p class="sr-only parrafoEstadoAudio">˙ltimo mensaje de estado: </p>');
-			divAlert = $('<div class="divAlertaAudio" role="alert" aria-live="assertive"></div>');
-			parrafoMensaje.append(divAlert);
-			wrapper.append(parrafoMensaje);
+		if (wrapper.find(".audioStateParagraph").length == 0) {
+			var msgParagraph = $('<p class="sr-only audioStateParagraph">' + _('√∫ltimo mensaje de estado:') + '</p>');
+			divAlert = $('<div class="divAlert" role="alert" aria-live="assertive"></div>');
+			msgParagraph.append(divAlert);
+			wrapper.append(msgParagraph);
 		}
 		else {
-			divAlert = wrapper.find(".divAlertaAudio");
+			divAlert = wrapper.find(".divAlert");
 		}
 		divAlert.html(text);
 	}, 1);
 }
 
-function agregaTitulo(titulo, prefijo, incluirSpanOculto) {
-	if (prefijo == null) prefijo = undefined;
-	if (incluirSpanOculto === undefined) incluirSpanOculto = true;
-	if (titulo !== undefined) {
-		return (incluirSpanOculto ? '<span class="sr-only">' : '') +
-			(prefijo !== undefined ? ' ' + prefijo + ' ' : '') +
-			' "' + titulo + '"' + (incluirSpanOculto ? '</span>' : '');
+// Return a string composed by prefix and title.
+// if includeHiddenSpan is true, the text is surrounded by a hidden span that is only visible to screen readers.
+// By default, includeHiddenSpan is true.
+function addTitle(title, prefix, includeHiddenSpan) {
+	if (prefix == null) prefix = undefined;
+	if (includeHiddenSpan === undefined) includeHiddenSpan = true;
+	if (title !== undefined) {
+		return (includeHiddenSpan ? '<span class="sr-only">' : '') +
+			(prefix !== undefined ? ' ' + prefix + ' ' : '') +
+			' "' + title + '"' + (includeHiddenSpan ? '</span>' : '');
 	}
 	else {
 		return '';
 	}
 }
 
-function getTiempo(duracion) {
-	if (duracion == 0) return "0 segundos";
-	var resultado = new Array();
-	if (duracion > 3600) {
-		var horas = parseInt(duracion / 3600);
-		resultado.push(horas.toString() + " hora" + (horas > 1 ? "s" : ""));
-		duracion %= 3600;
+function getStrTime(duration) {
+	if (duration == "Infinity") return _("Retransmitiendo");
+	if (duration === undefined) return _("desconocida");
+	if (duration == 0) return _('{0} segundos', '0');
+	var result = new Array();
+	if (duration > 3600) {
+		var h= parseInt(duration / 3600);
+		if (h > 1) result.push(_("{0} horas", h.toString()));
+		else result.push(_("{0} hora", hras.ToString()));
+		duration %= 3600;
 	}
-	if (duracion > 60) {
-		var minutos = parseInt(duracion / 60);
-		resultado.push(minutos.toString() + " minuto" + (minutos > 1 ? "s" : ""));
-		duracion %= 60;
+	if (duration > 60) {
+		var m = parseInt(duration / 60);
+		if (m > 1) result.push(_("{0} minutos", m.toString()));
+		else result.push(_("{0} minuto", m.toString()));
+		duration %= 60;
 	}
-	if (duracion > 0) {
-		resultado.push(parseInt(duracion).toString() + " segundo" + (duracion > 1 ? "s" : ""));
+	if (duration > 0) {
+		var s = parseInt(duration);
+		if (s > 1) result.push(_("{0} segundos", s.toString()));
+		else result.push(_("{0} segundo", s.toString()));
 	}
-	return (resultado.length > 1 ? resultado.slice(0, resultado.length - 1).join(', ') + ' y ' + resultado[resultado.length - 1] : resultado[0]);
+	return (result.length > 1 ? _("{0} y {1}", result.slice(0, result.length - 1).join(', '), result[result.length - 1])
+		: result[0]);
 }
 
-function cargaReproductores() {
+function preparePlayers() {
 	if (!window.HTMLAudioElement) {
-		alert("Imposible generar reproductores HTML5. Tu navegador no es compatible. Por favor, actualÌzalo.");
+		alert(_("Imposible generar reproductores HTML5. Tu navegador no es compatible. Por favor, actual√≠zalo."));
 		return;
 	}
-	$("audio").each(function (i, val) { cargaReproductor($(val)); });
+	$("audio").each(function (i, val) { preparePlayer($(val)); });
 }
 
-function cargaReproductor(audioElement) {
-	// Este reproductor ya est· cargado.
-	if (audioElement.parent().hasClass("wrapRepro")) return;
+function preparePlayer(audioElement) {
+	// If this player already has been processed to include accessibility layer and events, do not do anithing.
+	if (audioElement.parent().hasClass("playerWrapper")) return;
+	var audioNativeElement = audioElement[0];
 	audioElement.attr("preload", "auto").attr("aria-hidden", "true");
-	var wrap = $("<div class=\"wrapRepro\"></div");
+	var wrap = $("<div class=\"playerWrapper\"></div");
 	audioElement.wrap(wrap);
 	wrap = audioElement.parent();
-
-	var titulo = audioElement.attr("data-title");
-	audioElement = audioElement[0];
-	audioElement.onplay = function () {
-		accessibleAlert("Reproduciendo...", wrap);
-		wrap.find(".btnPlay").html("Pausar" + agregaTitulo(titulo, "el audio"));
-	};
-	audioElement.onplaying = function () {
-		$("audio").not($(audioElement)).each(function (i, val) { if (!val.paused) val.pause(); });
-
-		wrap.find(".btnPlay").html("Pausar" + agregaTitulo(titulo, "el audio"));
-	};
-	audioElement.oncanplay = function () {
-		wrap.find(".btnMute").html('Silenciar' + agregaTitulo(titulo, 'el audio'));
-		wrap.find(".ddDuracion").html(getTiempo(this.duration));
-	};
-	audioElement.ondurationchange = function () {
-		wrap.find(".ddDuracion").html(getTiempo(this.duration));
-	};
-	audioElement.onended = function () {
-		accessibleAlert("Audio finalizado.", wrap);
-		wrap.find(".btnPlay").html("Reproducir" + agregaTitulo(titulo, "el audio"));
-	};
-	audioElement.onseeked = function () {
-		accessibleAlert("Movido a la posici√≥n " + getTiempo(this.currentTime), wrap);
-	};
-	audioElement.onvolumechange = function () {
-		accessibleAlert("Volumen ajustado al " + (~~(this.volume * 100)).toString() + "%.", wrap);
-	};
-	audioElement.onerror = function () {
-		alert("Error al cargar el audio " + titulo + ". CÛdigo de error: " + audioElement.error.code + ".");
-	};
-	audioElement.ontimeupdate = function () {
-		wrap.find(".ddPosicion").html(getTiempo(this.currentTime));
-	};
-	audioElement.onpause = function () {
-		wrap.find(".btnPlay").html("Reproducir" + agregaTitulo(titulo, "el audio"));
-	};
-	agregaControlesAccesibles(wrap);
-}
-	
-
-function agregaControlesAccesibles(audioWrapper) {
-	var audioElement = audioWrapper.find("audio:first");
-	var titulo = audioElement.attr("data-title");
-	if (audioWrapper.find(".playerControls").length == 0) {
-		var btnPlay = $('<a href="#" class="btnPlayerControl btnPlay" >Reproducir' + agregaTitulo(titulo, 'el audio') + '</a>');
-		var btnMute = $('<a href="#" class="btnPlayerControl btnMute" >Desactivar / activar sonido' + agregaTitulo(titulo, 'al audio') + '</a>');
-		var btnVolumeDown = $('<a href="#" class="btnPlayerControl btnVolumeDown" >Bajar volumen' + agregaTitulo(titulo, 'al audio') + '</a>');
-		var btnVolumeUp = $('<a href="#" class="btnPlayerControl btnVolumeUp" >Subir volumen' + agregaTitulo(titulo, 'al audio') + '</a>');
-		var btnFw30Secs = $('<a href="#" class="btnPlayerControl btnFw30Secs" >Avanzar treinta segundos' + agregaTitulo(titulo, 'en el audio') + '</a>');
-		var btnBw30Secs = $('<a href="#" class="btnPlayerControl btnBw30Secs" >Retroceder treinta segundos' + agregaTitulo(titulo, 'en el audio') + '</a>');
-		var dlInfo = $('<dl></dl>');
-		var dtPosicion = $('<dt>Posici√≥n:</dt>');
-		var ddPosicion = $('<dd class=\"ddPosicion\">0 segundos</dd>');
-		var dtDuracion = $('<dt>Duraci√≥n:</dt>');
-		var ddDuracion = $('<dd class=\"ddDuracion\">Desconocida</dd>');
-		var ul = $("<ul></ul>"), liPlay = $('<li class="liControl"></li>'), liMute = $('<li class="liControl"></li>'), liVolumeDown = $('<li class="liControl"></li>'), liVolumeUp = $('<li class="liControl"></li>'), liFw30Secs = $('<li class="liControl"></li>'), liBw30Secs = $('<li class="liControl"></li>');
-		var capaControles = $("<div class=\"playerControls sr-only\"></div>");
-		capaControles.insertBefore(audioElement);
-		if (audioElement.attr("data-showDownloadLink") == "true") {
-			var url;
-			if (audioElement.attr("src") !== undefined) {
-				url = audioElement.attr("src");
-			}
-			else {
-				var source = audioElement.find("source[src]:first");
-				if (source.length > 0) {
-					url = source.attr("src");
-				}
-			}
-			if (url !== undefined) {
-				var lnkDescargar = $('<a class="lnkDescargar" href="' + url + '">Descargar' + agregaTitulo(titulo, "el audio", true) + '</a>');
-				var pLnkDescargar = $("<p></p>");
-				pLnkDescargar.append(lnkDescargar);
-				pLnkDescargar.insertBefore(capaControles);
-			}
-		}
-		capaControles.append(ul);
-		liPlay.append(btnPlay);
-		ul.append(liPlay);
-		// en iPhone, el volumen del audio no se puede controlar por javascript, asÌ que quitamos los botones
-		if (!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
-			liVolumeDown.append(btnVolumeDown);
-			ul.append(liVolumeDown);
-			liVolumeUp.append(btnVolumeUp);
-			ul.append(liVolumeUp);
-			liMute.append(btnMute);
-			ul.append(liMute);
-		}
-		liFw30Secs.append(btnFw30Secs);
-		ul.append(liFw30Secs);
-		liBw30Secs.append(btnBw30Secs);
-		ul.append(liBw30Secs);
-		dlInfo.append(dtPosicion);
-		dlInfo.append(ddPosicion);
-		dlInfo.append(dtDuracion);
-		dlInfo.append(ddDuracion);
-		dlInfo.insertAfter(ul);
-		// si no es iPhone, ponemos rol botÛn
-		if (!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) { ul.find(".btnPlayerControl").attr("role", "button"); }
+	if (audioNativeElement.readyState == AUDIO_HAVE_ENOUGH_DATA) {
+		addAccessibleControls(wrap);
 	}
+	audioNativeElement.oncanplay = function () {
+		addAccessibleControls(wrap);
+	}
+	audioNativeElement.onplay = function () {
+		var jqueryAudio = $(this);
+		var titleAudio = jqueryAudio.attr("data-title");
+		$("audio").not(jqueryAudio).each(function (i, val) { if (!val.paused) val.pause(); });
+		accessibleAlert(_("Reproduciendo..."), wrap);
+		wrap.find(".btnPlay").html(_("Pausar {0}", addTitle(titleAudio, _("el audio"))));
+	};
+	audioNativeElement.ondurationchange = function () {
+		wrap.find(".ddDuration").html(getStrTime(this.duration));
+	};
+	audioNativeElement.onended = function () {
+		accessibleAlert(_("Audio finalizado."), wrap);
+		var jqueryAudio = $(this);
+		var titleAudio = jqueryAudio.attr("data-title");
+		wrap.find(".btnPlay").html(_("Reproducir {0}", addTitle(titleAudio, _("el audio"))));
+	};
+	audioNativeElement.onseeked = function () {
+		accessibleAlert(_("Movido a la posici√≥n {0}", getStrTime(this.currentTime)), wrap);
+	};
+	audioNativeElement.onvolumechange = function () {
+		accessibleAlert(_("Volumen ajustado al {0}%.", (~~(this.volume * 100)).toString()), wrap);
+	};
+	audioNativeElement.onerror = function () {
+		var jqueryAudio = $(this);
+		var titleAudio = jqueryAudio.attr("data-title");
+		alert(_("Error al cargar el audio {0}. C√≥digo de error: {1}.", audioTitle, audioElement.error.code));
+	};
+	audioNativeElement.ontimeupdate = function () {
+		wrap.find(".ddPosition").html(getStrTime(this.currentTime));
+	};
+	audioNativeElement.onpause = function () {
+		var jqueryAudio = $(this);
+		var titleAudio = jqueryAudio.attr("data-title");
+		wrap.find(".btnPlay").html(_("Reproducir {0}", addTitle(titleAudio, _("el audio"))));
+		accessibleAlert(_("Pausado..."), wrap);
+
+	};
 }
 
-$(function() {
+
+function addAccessibleControls(audioWrapper) {
+	var audioElement = audioWrapper.find("audio:first");
+	var audioNativeElement = audioElement[0];
+	var title = audioElement.attr("data-title");
+	var controlsLayer = audioWrapper.find(".playerControls");
+	if (controlsLayer.length > 0) {
+		controlsLayer.remove();
+	}
+	var btnPlay = $('<a href="#" class="btnPlayerControl btnPlay" >' + _('Reproducir {0}', addTitle(title, _('el audio'))) + '</a>');
+	var btnMute = $('<a href="#" class="btnPlayerControl btnMute">' + _('Silenciar {0}', addTitle(title, _('el audio'))) + '</a>');
+	var btnVolumeDown = $('<a href="#" class="btnPlayerControl btnVolumeDown">' + _('Bajar volumen {0}', addTitle(title, _('al audio'))) + '</a>');
+	var btnVolumeUp = $('<a href="#" class="btnPlayerControl btnVolumeUp">' + _('Subir volumen {0}', addTitle(title, _('al audio'))) + '</a>');
+	var btnFw30Secs = $('<a href="#" class="btnPlayerControl btnFw30Secs">' + _('Avanzar treinta segundos {0}', addTitle(title, _('en el audio'))) + '</a>');
+	var btnBw30Secs = $('<a href="#" class="btnPlayerControl btnBw30Secs" >' + _('Retroceder treinta segundos {0}', addTitle(title, _('en el audio'))) + '</a>');
+	var dlInfo = $('<dl></dl>');
+	var dtPosition = $('<dt>' + _('Posici√≥n:') + '</dt>');
+	var ddPosition = $('<dd class="ddPosition">' + _('0 segundos') + '</dd>');
+	var dtDuration = $('<dt>' + _('Duraci√≥n:') + '</dt>');
+	var ddDuration = $('<dd class=\"ddDuration\">' + getStrTime(audioNativeElement.duration) + '</dd>');
+	var ul = $("<ul></ul>"), liPlay = $('<li class="liControl"></li>'), liMute = $('<li class="liControl"></li>'), liVolumeDown = $('<li class="liControl"></li>'), liVolumeUp = $('<li class="liControl"></li>'), liFw30Secs = $('<li class="liControl"></li>'), liBw30Secs = $('<li class="liControl"></li>');
+	controlsLayer = $("<div class=\"playerControls sr-only\"></div>");
+	controlsLayer.insertBefore(audioElement);
+	if (audioElement.attr("data-showDownloadLink") == "true") {
+		var url;
+		if (audioElement.attr("src") !== undefined) {
+			url = audioElement.attr("src");
+		}
+		else {
+			var source = audioElement.find("source[src]:first");
+			if (source.length > 0) {
+				url = source.attr("src");
+			}
+		}
+		if (url !== undefined) {
+			var lnkDownload = $('<a class="lnkDownload" href="' + url + '">' + _('Descargar {0}', addTitle(title, _('el audio'))) + '</a>');
+			var pLnkDescargar = $("<p></p>");
+			pLnkDescargar.append(lnkDownload);
+			pLnkDescargar.insertBefore(controlsLayer);
+		}
+	}
+	controlsLayer.append(ul);
+	liPlay.append(btnPlay);
+	ul.append(liPlay);
+	// In iOS, the audio volume can not be controlled by javascript, so remove buttons
+	if (!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+		liVolumeDown.append(btnVolumeDown);
+		ul.append(liVolumeDown);
+		liVolumeUp.append(btnVolumeUp);
+		ul.append(liVolumeUp);
+		liMute.append(btnMute);
+		ul.append(liMute);
+	}
+	liFw30Secs.append(btnFw30Secs);
+	ul.append(liFw30Secs);
+	liBw30Secs.append(btnBw30Secs);
+	ul.append(liBw30Secs);
+	dlInfo.append(dtPosition);
+	dlInfo.append(ddPosition);
+	dlInfo.append(dtDuration);
+	dlInfo.append(ddDuration);
+	dlInfo.insertAfter(ul);
+	// If it isn't iPhone, we add role "button" to all player controls link.
+	if (!navigator.userAgent.match(/(iPod|iPhone|iPad)/)) { ul.find(".btnPlayerControl").attr("role", "button"); }
+}
+
+
+$(function () {
 	$("html").on("click", ".btnPlay", function (e) {
 		e.preventDefault();
-		var wraper = $(this).parents(".wrapRepro");
+		var wraper = $(this).parents(".playerWrapper");
 		var audioElement = wraper.find("audio");
 		audioElement = audioElement[0];
 		if (audioElement.paused) {
@@ -188,23 +214,24 @@ $(function() {
 			audioElement.pause();
 		}
 	});
+	
 	$("html").on("click", ".btnMute", function (e) {
 		e.preventDefault();
-		var wraper = $(this).parents(".wrapRepro");
+		var wraper = $(this).parents(".playerWrapper");
 		var audioElement = wraper.find("audio");
-		var titulo = audioElement.attr("data-title");
+		var title = audioElement.attr("data-title");
 		audioElement = audioElement[0];
 		if (audioElement.muted) {
-			$(this).html('Silenciar' + agregaTitulo(titulo, 'el audio'));
+			$(this).html(_('Silenciar {0}', addTitle(title, _('el audio'))));
 		}
 		else {
-			$(this).html('Activar sonido' + agregaTitulo(titulo, 'al audio'));
+			$(this).html(_('Activar sonido {0}', addTitle(title, _('al audio'))));
 		}
 		audioElement.muted = !audioElement.muted;
 	});
 	$("html").on("click", ".btnVolumeUp", function (e) {
 		e.preventDefault();
-		var wrapper = $(this).parents(".wrapRepro");
+		var wrapper = $(this).parents(".playerWrapper");
 		var audioElement = wrapper.find("audio")[0];
 		if (audioElement.volume == 100.0) return;
 		if (audioElement.volume > 0.9) audioElement.volume = 1;
@@ -213,31 +240,32 @@ $(function() {
 
 	$("html").on("click", ".btnVolumeDown", function (e) {
 		e.preventDefault();
-		var wrapper = $(this).parents(".wrapRepro:first");
+		var wrapper = $(this).parents(".playerWrapper:first");
 		var audioElement = wrapper.find("audio")[0];
 		if (audioElement.volume == 0.0) return;
 		if (audioElement.volume < 0.1) audioElement.volume = 0;
 		else audioElement.volume -= 0.1;
 	});
+	
 	$("html").on("click", ".btnFw30Secs", function (e) {
 		e.preventDefault();
-		var wrapper = $(this).parents(".wrapRepro:first");
+		var wrapper = $(this).parents(".playerWrapper:first");
 		var audioElement = wrapper.find("audio")[0];
 		if (audioElement.currentTime + 30 > audioElement.duration) audioElement.currentTime = audioElement.duration;
 		else audioElement.currentTime += 30;
 	});
 	$("html").on("click", ".btnBw30Secs", function (e) {
 		e.preventDefault();
-		var wrapper = $(this).parents(".wrapRepro:first");
+		var wrapper = $(this).parents(".playerWrapper:first");
 		var audioElement = wrapper.find("audio")[0];
 		if (audioElement.currentTime - 30 < 0) audioElement.currentTime = 0;
 		else audioElement.currentTime -= 30;
 	});
 	$("html").on("focus", "a.btnPlayerControl", function () {
-			$(this).parents(".playerControls").removeClass("sr-only");
-	}); 
+		$(this).parents(".playerControls").removeClass("sr-only");
+	});
 	$("html").on("blur", "a.btnPlayerControl", function () {
 		$(this).parents(".playerControls").addClass("sr-only");
 	});
-	cargaReproductores();
+	preparePlayers();
 });
